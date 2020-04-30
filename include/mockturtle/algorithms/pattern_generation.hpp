cd0303badd6a3fb0cc81552e50bf562c32f66a3d
Sonia <lee30sonia@gmail.com>
2020-04-30 22:06:57 +0200

    not reusing var id

--- include/mockturtle/algorithms/pattern_generation.hpp
+++ include/mockturtle/algorithms/pattern_generation.hpp
@@ -172,6 +172,7 @@ public:
     generate_cnf<Ntk>( ntk, [&]( auto const& clause ) {
       solver.add_clause( clause );
     }, literals );
+    max_id = ntk.size() - 1;
 
     call_with_stopwatch( st.time_sim, [&]() {
       simulate_nodes<Ntk>( ntk, tts, sim );
@@ -189,6 +190,12 @@ public:
   }
 
 private:
+  uint32_t new_lit()
+  {
+    solver.add_var();
+    return make_lit( ++max_id );
+  }
+
   void add_clauses_for_gate( node const& n, unordered_node_map<uint32_t, Ntk> const& lits, bool inverse = false )
   {
     /* currently only consider AIG */
@@ -224,8 +231,7 @@ private:
       if ( ntk.visited( fo ) == ntk.trav_id() ) return true; /* skip */
       ntk.set_visited( fo, ntk.trav_id() );
 
-      solver.add_var();
-      lits[fo] = make_lit( solver.nr_vars() - 1 );
+      lits[fo] = new_lit();
       
       make_lit_fanout_cone_rec( fo, lits );
       return true; /* next */
@@ -245,8 +251,7 @@ private:
     ntk.foreach_fanin( n, [&]( auto const& fi ){
       lits[fi] = literals[fi];
     });
-    solver.add_var();
-    lits[n] = make_lit( solver.nr_vars() - 1 );
+    lits[n] = new_lit();
     ntk.incr_trav_id();
     make_lit_fanout_cone_rec( n, lits );
 
@@ -265,8 +270,7 @@ private:
       const auto a = lit_not_cond( literals[f], ntk.is_complemented( f ) );
       const auto b = lit_not_cond( lits[f], ntk.is_complemented( f ) );
 
-      solver.add_var();
-      const auto c = make_lit( solver.nr_vars() - 1 );
+      const auto c = new_lit();
       miter.emplace_back( c );
 
       solver.add_clause( {lit_not( a ), lit_not( b ), lit_not( c )} );
@@ -277,8 +281,7 @@ private:
       return true; /* next */
     });
     assert( miter.size() > 0 );
-    solver.add_var();
-    const auto nlit = make_lit( solver.nr_vars() - 1 );
+    const auto nlit = new_lit();
     miter.emplace_back( nlit );
     assumptions.emplace_back( lit_not( nlit ) );
     solver.add_clause( miter );
@@ -587,8 +590,7 @@ private:
 
         if ( tts[root] == tts[n] || ~tts[root] == tts[n] )
         {
-          solver.add_var();
-          auto nlit = make_lit( solver.nr_vars()-1 );
+          auto nlit = new_lit();
           solver.add_clause( {literals[root], literals[n], nlit} );
           solver.add_clause( {literals[root], lit_not( literals[n] ), lit_not( nlit )} );
           solver.add_clause( {lit_not( literals[root] ), literals[n], lit_not( nlit )} );
@@ -631,6 +633,7 @@ private:
 
   node_map<uint32_t, Ntk> literals;
   percy::bsat_wrapper solver;
+  uint32_t max_id;
   
   TT tts;
 
